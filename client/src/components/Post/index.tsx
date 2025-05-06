6920c59d1fe927a124805ff997e5c67947e6cc78
artemats <artem.matseruk@gmail.com>
2025-05-06 14:11:01 +0300

    feat: started work with profile page

--- client/src/components/Post/index.tsx
+++ client/src/components/Post/index.tsx
@@ -11,15 +11,16 @@ const Post = ({ data }: { data: IPost }) => {
         <Avatar className="h-12 w-12 min-w-12">
           <AvatarImage src={data.picturePath} />
           <AvatarFallback className="bg-zinc-300 dark:bg-zinc-700 user-select-none">
-            {data.firstName.split('')[0] + data.lastName.split('')[0]}
+            {data.author.firstName.split('')[0] +
+              data.author.lastName.split('')[0]}
           </AvatarFallback>
         </Avatar>
         <div>
           <p className="text-md font-bold">
-            {data.firstName} {data.lastName}
+            {data.author.firstName} {data.author.lastName}
           </p>
           <p className="text-sm text-zinc-500 dark:text-zinc-400 leading-none">
-            {data.location}
+            {data.author.location}
           </p>
         </div>
       </div>
@@ -29,18 +30,22 @@ const Post = ({ data }: { data: IPost }) => {
       <div className="min-h-[350px] bg-muted overflow-hidden rounded-lg">
         <img
           src="https://images.unsplash.com/photo-1549737525-45c81177eef7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt={`${data.firstName} ${data.lastName}`}
+          alt={`${data.author.firstName} ${data.author.lastName}`}
           className="object-cover object-center"
         />
       </div>
       <div className="flex items-center gap-9 pt-4">
         <div className="flex items-center gap-2">
           <Heart className="w-5 h-5" />
-          <span className="text-sm text-zinc-500 dark:text-zinc-400">0</span>
+          <span className="text-sm text-zinc-500 dark:text-zinc-400">
+            {data.likes.length}
+          </span>
         </div>
         <div className="flex items-center gap-2">
           <MessageSquareText className="w-5 h-5" />
-          <span className="text-sm text-zinc-500 dark:text-zinc-400">0</span>
+          <span className="text-sm text-zinc-500 dark:text-zinc-400">
+            {data.comments.length}
+          </span>
         </div>
       </div>
     </WidgetWrapper>
